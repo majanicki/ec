1a4dc1f178fd8da61c34dea258455dbd8ef19d0a
Maciej <maciej.ja@proton.me>
2025-10-13 12:50:17 +0200

    feat: final

--- lab01/main.cpp
+++ lab01/main.cpp
@@ -430,7 +430,7 @@ void save_solution_to_txt(Solution solution, const std::string& filename) {
         panicf("Failed to open a file %s\n", filename.c_str());
     }
     for (size_t i = 0; i < solution.size(); i++) {
-        file << solution[i].id << "\n";
+        file << solution[i].id << ", ";
     }
     file << solution[0].id << "\n";
     infof("Saved %s", filename.c_str());
@@ -462,7 +462,7 @@ void print_stats(const std::vector<Solution>& solutions, const std::vector<Node>
     save_solution_to_png(best_solution, dataset ,name + ".png");
 }
 
-void benchmark_solutions(const std::vector<Node> dataset, CostMatrix dist) {
+void benchmark_solutions(const std::vector<Node> dataset, CostMatrix dist, const std::string& suffix) {
     std::vector<Solution> solutions_random;
     std::vector<Solution> solutions_nearest_neighbor_end_only;
     std::vector<Solution> solutions_nearest_neighbor_every_position;
@@ -480,13 +480,13 @@ void benchmark_solutions(const std::vector<Node> dataset, CostMatrix dist) {
         Solution greedy_cycle_solution = get_greedy_cycle(dataset, dist, i);
         solutions_greedy_cycle.push_back(greedy_cycle_solution);
     }
-    print_stats(solutions_random, dataset, dist, "random");
+    print_stats(solutions_random, dataset, dist, "random"+suffix);
     std::cout << std::endl;
-    print_stats(solutions_nearest_neighbor_end_only, dataset, dist, "nearest_neighbor_end_only");
+    print_stats(solutions_nearest_neighbor_end_only, dataset, dist, "nearest_neighbor_end_only"+suffix);
     std::cout << std::endl;
-    print_stats(solutions_nearest_neighbor_every_position, dataset, dist, "nearest_neighbor_all_positions");
+    print_stats(solutions_nearest_neighbor_every_position, dataset, dist, "nearest_neighbor_all_positions"+suffix);
     std::cout << std::endl;
-    print_stats(solutions_greedy_cycle, dataset, dist, "greedy_cycle");
+    print_stats(solutions_greedy_cycle, dataset, dist, "greedy_cycle"+suffix);
 
 }
 
@@ -504,6 +504,12 @@ int main() {
     InitWindow(1, 1, "This is a title");
     SetWindowState(FLAG_WINDOW_HIDDEN);
 
-    benchmark_solutions(dataset, dist);
+    benchmark_solutions(dataset, dist, "_a");
+
+    whole_file = read_file("./TSPB.csv");
+    std::vector<Node> dataset_b = parse_dataset(whole_file);
+    dist = compute_distance_matrix(dataset_b);
+
+    benchmark_solutions(dataset_b, dist, "_b");
     return 0;
 }
